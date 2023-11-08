d496c1805822b51ed28b5d5d534c0ad4257f2182
Raj Majumdar <92665411+raj23689@users.noreply.github.com>
2023-11-08 14:36:57 +0530

    Film by page and length.

--- src/main/java/code/lazy/kick/model/Repository/FilmRepository.java
+++ src/main/java/code/lazy/kick/model/Repository/FilmRepository.java
@@ -7,6 +7,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @ApplicationScoped
 public class FilmRepository {
@@ -14,9 +15,23 @@ public class FilmRepository {
     @Inject
     JPAStreamer jpaStreamer;
 
+    private static final int PAGE_SIZE;
+
+    static {
+        PAGE_SIZE = 20;
+    }
+
     public Optional<Film> getFilm(short filmId) {
         return jpaStreamer.stream(Film.class)
                 .filter(Film$.filmId.equal(filmId))
                 .findFirst();
+    }
+
+    public Stream<Film> paged(long page, short minLength) {
+        return jpaStreamer.stream(Film.class)
+                .filter(Film$.length.greaterThan(minLength))
+                .sorted(Film$.length)
+                .skip(page * PAGE_SIZE)
+                .limit(PAGE_SIZE);
     }
 }
