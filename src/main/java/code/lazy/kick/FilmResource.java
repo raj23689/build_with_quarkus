f8e9069a1af8f2c053e8ede453ccb54e8ac062ab
Raj Majumdar <92665411+raj23689@users.noreply.github.com>
2023-11-08 15:37:04 +0530

    Implemented film by page and legth.

--- src/main/java/code/lazy/kick/FilmResource.java
+++ src/main/java/code/lazy/kick/FilmResource.java
@@ -9,6 +9,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Path(value = "/")
 public class FilmResource {
@@ -29,5 +30,14 @@ public class FilmResource {
     public String getFilm(short filmId) {
         Optional<Film> film = filmRepository.getFilm(filmId);
         return film.isPresent() ? film.get().getTitle() : "No film was found!";
+    }
+
+    @GET
+    @Path("/pagedFilms/{page}/{minLength}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String paged(long page, short minLength) {
+        return filmRepository.paged(page, minLength)
+                .map(f -> String.format("%s (%d min)", f.getTitle(), f.getLength()))
+                .collect(Collectors.joining("\n"));
     }
 }
