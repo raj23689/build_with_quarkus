2eac369babc4afbe17ff91575fef125c7a18dfba
Raj Majumdar <92665411+raj23689@users.noreply.github.com>
2023-11-08 14:12:15 +0530

    Film by ID filter with jpastreamer.

--- src/main/java/code/lazy/kick/model/Repository/FilmRepository.java
+++ src/main/java/code/lazy/kick/model/Repository/FilmRepository.java
@@ -1,4 +1,22 @@
 package code.lazy.kick.model.Repository;
 
+import code.lazy.kick.model.Film;
+import code.lazy.kick.model.Film$;
+import com.speedment.jpastreamer.application.JPAStreamer;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import java.util.Optional;
+
+@ApplicationScoped
 public class FilmRepository {
+
+    @Inject
+    JPAStreamer jpaStreamer;
+
+    public Optional<Film> getFilm(short filmId) {
+        return jpaStreamer.stream(Film.class)
+                .filter(Film$.filmId.equal(filmId))
+                .findFirst();
+    }
 }
