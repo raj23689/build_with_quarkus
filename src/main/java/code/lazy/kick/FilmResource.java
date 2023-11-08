bd02017c6f8fbc1645fe2008248ce7c8ba74524a
Raj Majumdar <92665411+raj23689@users.noreply.github.com>
2023-11-08 14:19:38 +0530

    Implemented Film by ID path.

--- src/main/java/code/lazy/kick/FilmResource.java
+++ src/main/java/code/lazy/kick/FilmResource.java
@@ -1,17 +1,33 @@
 package code.lazy.kick;
 
+import code.lazy.kick.model.Film;
+import code.lazy.kick.model.Repository.FilmRepository;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.Optional;
+
 @Path(value = "/")
 public class FilmResource {
+
+    @Inject
+    FilmRepository filmRepository;
 
     @GET
     @Path(value = "/helloWorld")
     @Produces(value = MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello, World!!";
+    }
+
+    @GET
+    @Path(value = "/film/{filmId}")
+    @Produces(value = MediaType.TEXT_PLAIN)
+    public String getFilm(short filmId) {
+        Optional<Film> film = filmRepository.getFilm(filmId);
+        return film.isPresent() ? film.get().getTitle() : "No film was found!";
     }
 }
