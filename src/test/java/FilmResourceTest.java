e98f38192b8c52343a38c56a49efb31de01a3713
Raj Majumdar <92665411+raj23689@users.noreply.github.com>
2023-11-08 14:33:03 +0530

    Another test method for filmbyId.

--- src/test/java/FilmResourceTest.java
+++ src/test/java/FilmResourceTest.java
@@ -15,4 +15,13 @@ public class FilmResourceTest {
                 .statusCode(200)
                 .body(containsString("ACADEMY DINOSAUR"));
     }
+
+    @Test
+    public void filmById2Test() {
+        given()
+                .when().get("/film/180")
+                .then()
+                .statusCode(200)
+                .body(containsString("CONSPIRACY SPIRIT"));
+    }
 }
